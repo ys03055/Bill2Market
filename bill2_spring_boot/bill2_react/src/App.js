f8c9187adddc22a9e2792b6d1411e51cf802c7d5
ys03055 <33304927+ys03055@users.noreply.github.com>
2022-03-27 00:51:43 +0900

    로그인. 회원가입

--- bill2_spring_boot/bill2_react/src/App.js
+++ bill2_spring_boot/bill2_react/src/App.js
@@ -1,32 +1,24 @@
+import "antd/dist/antd.css";   //antd 라이브러리 불러오기
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from "react";
-// import Axios from "axios";
+import {Switch, Route} from 'react-router-dom'
+import Loginpage from "./login/login.js";
+import SignUpPage from "./signup/signup";   //리엑트 컴포넌트는 항상 대문자로 시작해야 불러올 수 있습니다.
+                                            //이거 하나때문에 디버깅하는데 3시간 날림 ㅋ.ㅋ
 
 function App() {
-    const [user,setUser] = useState([]);
-    useEffect(() => {
-      fetch("/api/example").then((response) => {
-          return response.json();
-        })
-        .then(function (data) {
-          setUser(data);
-        });
-  }, []);
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            asdasda
-          </p>
-          <ul>
-             {user.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
-          </ul>
-        </header>
-      </div>
-   );
+  return (
+  <div>
+    <Switch>
+      <Route exact = {true} path ="/">
+        <Loginpage />
+      </Route>
+      <Route exact = {true} path = "/signup">
+        <SignUpPage />
+      </Route>
+    </Switch>
+    </div>
+  );
 }
 
 export default App;
