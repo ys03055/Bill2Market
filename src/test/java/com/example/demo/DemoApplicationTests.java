cbdd2cc2eb81ed71c5c020b6b730b056de0266b8
mi2ntae <73164347+mi2ntae@users.noreply.github.com>
2022-03-29 16:13:36 +0900

    <오류 수정> Test코드 삭제

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.model.Student;
-import com.example.demo.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
