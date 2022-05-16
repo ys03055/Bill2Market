ac49ad3b4f49ef7dc532ade19d9683b0824d0530
hanmin <hm5395@naver.com>
2022-05-16 23:14:52 +0900

    chatsClient - itemId추가를 통한 이슈 해결

--- src/main/java/com/example/demo/model/chat/ChatListResponseDTO.java
+++ src/main/java/com/example/demo/model/chat/ChatListResponseDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class ChatListResponseDTO {
 
-    private Integer itemId;
+    private Integer itemId; //itemId추가
     private Integer chatId;
     private Integer opponentIndex;
     private String fileName ;
